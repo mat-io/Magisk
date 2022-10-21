74025c6c3b777361c6dc87be31784711ea3bb915
Mateusz Bartosiak <mateusz.bartosiak@users.noreply.github.com>
2022-10-21 20:22:10 +0200

    Disable safeboot bypass

--- native/jni/core/bootstages.cpp
+++ native/jni/core/bootstages.cpp
@@ -311,7 +311,7 @@ void post_fs_data(int client) {
         goto early_abort;
     }
 
-    if (getprop("persist.sys.safemode", true) == "1" || check_key_combo()) {
+    if (getprop("persist.sys.safemode", true) == "1") {
         safe_mode = true;
         // Disable all modules and denylist so next boot will be clean
         disable_modules();
